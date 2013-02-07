de6230aa47fc5d9da6c7b6d6f72a66464ab4053d
Martin Brochhaus <mbrochh@gmail.com>
2013-02-07 17:02:19 +0800

    Removed static/ from .gitignore

---
+++ hero_slider/static/hero_slider/js/generic_raw_id_field.js
@@ -0,0 +1,21 @@
+var $ = django.jQuery
+$(document).ready(function(){
+    $('#id_object_id').parent().append('<div id="lookup_box"></div>');
+    $('#id_content_type').change(function () {
+        var $selected = $('#id_content_type option:selected')
+        var type = $selected.text();
+        var pk = $selected.val();
+        if (type === '---------') {
+            $('#lookup_box').html('Select content type first...');
+        } else {
+            $.get(
+                get_ctype_url + '?pk=' + pk,
+                function(data) {
+                    $('#lookup_box').html('<a id="lookup_id_object_id" class="related-lookup" onclick="return showRelatedObjectLookupPopup(this);"></a>');
+                    $('#lookup_id_object_id').html('<img src="'+ static_url +'/admin/img/selector-search.gif"/> Lookup ' + type);
+                    $('#lookup_id_object_id').attr('href', '/admin/'+ data.app_label +'/'+ data.model +'/?t=id');
+                }
+            )
+        }
+    }).change();
+});
